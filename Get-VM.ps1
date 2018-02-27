4455a1c99a308b6d2ef69fe636d43fe1eb4549ea
Joey Piccola <joey@joeypiccola.com>
2018-02-27 12:29:15 -0700

    oh

--- Get-VM.ps1
+++ Get-VM.ps1
@@ -13,5 +13,5 @@ Param (
 Get-Module -ListAvailable VMware* | Import-Module | Out-Null
 Connect-VIServer -Server vcenter -User $usr -Password $psw
 $vm = Get-VM nuget
-Write-Output $vm.name
+Write-Output $vmname
 Disconnect-VIServer -Force -Server vcenter -Confirm:$false
