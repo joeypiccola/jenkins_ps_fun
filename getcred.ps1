152b3836a69eeb5027e7209ab3649dfed7031a39
joeypiccola <joey.piccola@gmail.com>
2018-03-01 13:38:03 -0700

    clean up

--- getcred.ps1
+++
@@ -1,8 +0,0 @@
-﻿$credentials = [PSCustomObject]@{
-    vcenter_user = 'vcenter_user'
-    vcenter_pass = 'vcenter_pass'
-    ad_user      = 'ad_user'
-    ad_pass      = 'ad_user'
-}
-
-Write-Output $credentials
