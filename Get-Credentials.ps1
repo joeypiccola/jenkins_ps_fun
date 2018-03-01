f04a63b3e61bfb2e15e97a7bcfd489fe9307b592
joeypiccola <joey.piccola@gmail.com>
2018-03-01 13:30:30 -0700

    ad fixup

--- Get-Credentials.ps1
+++ Get-Credentials.ps1
@@ -18,7 +18,7 @@ $credentials = [PSCustomObject]@{
     vcenter_user = $vcenter_user
     vcenter_pass = $vcenter_pass
     ad_user      = $ad_user
-    ad_pass      = $ad_user
+    ad_pass      = $ad_pass
 }
 
 Write-Output $credentials
