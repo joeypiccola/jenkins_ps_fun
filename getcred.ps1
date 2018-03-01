d7d4062d69a74cd384182612d760e03e466cee48
joeypiccola <joey.piccola@gmail.com>
2018-03-01 11:53:41 -0700

    file wrong

---
+++ getcred.ps1
@@ -0,0 +1,8 @@
+﻿$credentials = [PSCustomObject]@{
+    vcenter_user = 'vcenter_user'
+    vcenter_pass = 'vcenter_pass'
+    ad_user      = 'ad_user'
+    ad_pass      = 'ad_user'
+}
+
+Write-Output $credentials
