e9c7d2289ddee9755ffd2319a36ebb2586ba39de
joeypiccola <joey.piccola@gmail.com>
2018-02-28 17:40:06 -0700

    more

--- Invoke-PrerequsitesChecks.ps1
+++ Invoke-PrerequsitesChecks.ps1
@@ -19,22 +19,30 @@ Param (
     [string]$vmname        
 )
 
-Get-Module -ListAvailable VMware* | Import-Module | Out-Null
-Connect-VIServer -Server $vcenter -User $vcenter_user -Password $vcenter_pass -erroraction stop
+$vmquery = $true
+$adquery = $true
 
 try {
+    $vcentersecpasswd = ConvertTo-SecureString $vcenter_pass -AsPlainText -Force
+    $vcentercreds = New-Object System.Management.Automation.PSCredential ($vcenter_user, $vcentersecpasswd)
+    Get-Module -ListAvailable VMware* | Import-Module | Out-Null
+    Connect-VIServer -Server $vcenter -Credential $vcentercreds -ErrorAction Stop
     $vm = Get-VM -Name $vmname -ErrorAction Stop
 } catch {
     Disconnect-VIServer -Server $vcenter -Confirm:$false -Force
     $vmquery = $false
-    Write-Error $_.Exception.Message
+    Write-Warning $_.Exception.Message
 }
 
 try {
-    $secpasswd = ConvertTo-SecureString $adjoin_pass -AsPlainText -Force
-    $mycreds = New-Object System.Management.Automation.PSCredential ($adjoin_user, $secpasswd)
-    $ad = Get-ADComputer -Identity $vmname -ErrorAction Stop -Credential $mycreds
+    $adsecpasswd = ConvertTo-SecureString $adjoin_pass -AsPlainText -Force
+    $adcreds = New-Object System.Management.Automation.PSCredential ($adjoin_user, $adsecpasswd)
+    $ad = Get-ADComputer -Identity $vmname -ErrorAction Stop -Credential $adcreds
 } catch {
     $adquery = $false
-    Write-Error $_.Exception.Message
+    Write-Warning $_.Exception.Message
+}
+
+if (($adquery -eq $true) -or ($vmquery -eq $true)) {
+    Write-Error 'object exists'
 }
