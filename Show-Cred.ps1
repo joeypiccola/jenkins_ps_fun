c291d88ac383ad2b4225e177e17af6f37466099b
joeypiccola <joey.piccola@gmail.com>
2018-03-01 12:36:43 -0700

    oi

---
+++ Show-Cred.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+Param (
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vcenter_user
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vcenter_pass
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$ad_user	
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$ad_pass
+)
+
+Write-Host "vcenter user is: $vcenter_user"
+Write-Host "vcenter pass is: $vcenter_pass"
+Write-Host "ad user is: $ad_user"
+Write-Host "ad psss is: $ad_pass"
+write-host "to host"
+write-output "to output"
