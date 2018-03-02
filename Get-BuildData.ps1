07e89c697a3041b54be10439dd06f8b7cea57352
joeypiccola <joey.piccola@gmail.com>
2018-03-02 10:46:47 -0700

    erg

--- Get-BuildData.ps1
+++ Get-BuildData.ps1
@@ -1,8 +1,11 @@
+
+$builddata = Get-Content .\builddata.json | ConvertFrom-Json
+
 # take 'ad.contoso.com' and make it 'adcontosocom'
 # remove all dots from win_domain var
 # generate the env var $env:cred_ad_adcontosocom_USR and get the value
 # generate the env var $env:cred_ad_adcontosocom_PSW and get the value
-$ad_key = ($env:win_domain.replace('.',''))
+$ad_key = ($builddata.win_domain.replace('.',''))
 $ad_user = (Get-ChildItem -Path ('env:cred_ad_' + $ad_key + '_USR')).value
 $ad_pass = (Get-ChildItem -Path ('env:cred_ad_' + $ad_key + '_PSW')).value
 
@@ -10,11 +13,11 @@ $ad_pass = (Get-ChildItem -Path ('env:cred_ad_' + $ad_key + '_PSW')).value
 # remove all dots and hostname from vcenter var
 # generate the env var $env:cred_vcenter_dallascontosocom_USR and get the value
 # generate the env var $env:cred_vcenter_dallascontosocom_PSW and get the value
-$vcenter_key = ($env:vcenter -replace '^.+?\.(.+)$','$1').Replace('.','')
+$vcenter_key = ($builddata:vcenter -replace '^.+?\.(.+)$','$1').Replace('.','')
 $vcenter_user = (Get-ChildItem -Path ('env:cred_vcenter_' + $vcenter_key + '_USR')).value
 $vcenter_pass = (Get-ChildItem -Path ('env:cred_vcenter_' + $vcenter_key + '_PSW')).value
 
-switch ($env:win_domain) {
+switch ($builddata:win_domain) {
     'ad.piccola.us' {
         $ou = 'OU=Test,OU=LabStuff,OU=Servers,DC=ad,DC=piccola,DC=us'
     }
@@ -22,8 +25,6 @@ switch ($env:win_domain) {
         $ou = 'OU=Test,OU=Servers,DC=cis,DC=com'
     }
 }
-
-$builddata = Get-Content .\builddata.json | ConvertFrom-Json
 
 $envdata = [PSCustomObject]@{
     vcenter_user = $vcenter_user
