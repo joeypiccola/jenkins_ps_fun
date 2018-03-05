b69e25918b6ca315b2fdc08e83de64ed3dcc5ff0
joeypiccola <joey.piccola@gmail.com>
2018-03-05 15:14:43 -0700

    added remove

---
+++ Remove-OSCustomizationSpec.ps1
@@ -0,0 +1,31 @@
+﻿[CmdletBinding()]
+Param (
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vcenter_user
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vcenter_pass
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vcenter
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$cspec_name
+)
+
+$ErrorActionPreference = 'Stop'
+
+$vcenter_pass_sec = ConvertTo-SecureString $vcenter_pass -AsPlainText -Force
+$vcenter_cred = New-Object System.Management.Automation.PSCredential ($vcenter_user, $vcenter_pass_sec)
+Get-Module -ListAvailable VMware* | Import-Module
+Connect-VIServer -Server $vcenter -Credential $vcenter_cred
+
+try {
+    $get = Get-OSCustomizationSpec -Name $cspec_name
+    $get | Remove-OSCustomizationSpec -Confirm:$false
+    Disconnect-VIServer -Force -Confirm:$false
+} catch {
+    Disconnect-VIServer -Force -Confirm:$false
+    Write-Error $_.Exception.Message
+    exit 1
+}
