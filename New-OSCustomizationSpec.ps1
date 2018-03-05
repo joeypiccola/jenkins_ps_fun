e9580d533b4226282c7c6156159228bdd527f1da
joeypiccola <joey.piccola@gmail.com>
2018-03-05 12:03:57 -0700

    new spec added

--- New-OSCustomizationSpec.ps1
+++ New-OSCustomizationSpec.ps1
@@ -25,6 +25,9 @@ Param (
     [string]$vmname
     ,
     [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$role
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
     [string]$win_domain
     ,
     [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
@@ -37,26 +40,33 @@ Param (
     [pscustomobject]$networking_cfg
 )
 
+# take the provided disk config and convert it to json string
 $disk_cfg_file_contents = switch ($disk_cfg.disks.Count) {
     {$PSItem -gt 0} {
         Write-Output $disk_cfg.disks | ConvertTo-Json -Compress
     }
     default {
-        Write-Output 'no disks'
+        Write-Output 'nodisks'
     }
 }
 
+# take the provided networking config and convert it to json string
 $networking_cfg_file_contents = $networking_cfg | ConvertTo-Json -Compress
 
+# take the provided role and replace the spaces with an underscore
+$role_script = $role.Replace(' ','_').ToLower()
+
+# build an array of run once commands 
+$selectedConfigRunOnce = @(
+    "cmd /c echo $networking_cfg_file_contents >> c:\deploy\netcfg.json",
+    "cmd /c echo $disk_cfg_file_contents >> c:\deploy\diskcfg.json",
+    "c:\deploy\callps.bat command `"iwr http://nuget.ad.piccola.us:8081/chocogit.ps1 -UseBasicParsing | iex`"",
+    "c:\deploy\callps.bat command `"& 'c:\Program Files\Git\cmd\git.exe' clone -b master https://github.com/joeypiccola/vmware-runonce.git c:\deploy\config`"",
+    "c:\deploy\callps.bat file `"c:\deploy\config\$win_domain\$role_script.ps1`""
+)
+
+# build a param splat based on the provided win_domain for the vmware customization spec
 if ($win_domain -eq 'workgroup') {
-    $selectedConfigRunOnce = @(
-        "cmd /c echo $networking_cfg_file_contents >> c:\deploy\netcfg.json",
-        "cmd /c echo $disk_cfg_file_contents >> c:\deploy\diskcfg.json",
-        "c:\deploy\callps.bat command `"iwr https://artifactory/artifactory/win-binaries/scripts/vmwarerunonce/chocogit.ps1 -UseBasicParsing | iex`"",
-        "c:\deploy\callps.bat command `"& 'c:\Program Files\Git\cmd\git.exe' clone -b qa https://u:p@bitbucket/scm/iwp/runonce-vmware.git c:\deploy\config`"",
-        "c:\deploy\callps.bat file `"c:\deploy\config\ms_non_dva_corp.ps1`""
-    )
-    
     $specSplat = @{
         Name                  = $cspec_name
         OSType                = 'Windows'
@@ -73,19 +83,39 @@ if ($win_domain -eq 'workgroup') {
         NamingScheme          = 'Fixed'
         NamingPrefix          = $vmname
     }
+} else {
+    $specSplat = @{
+        Name                  = $cspec_name
+        OSType                = 'Windows'
+        ChangeSid             = $true
+        OrgName               = $win_domain
+        FullName              = $workgroup_user
+        AdminPassword         = $workgroup_pass
+        AutoLogonCount        = 2
+        GuiRunOnce            = $selectedConfigRunOnce
+        Description           = 'this is a dyn generated on the fly customization spec'
+        Domain                = $win_domain
+        DomainUserName        = $ad_user
+        DomainPassword        = $ad_pass
+        LicenseMode           = 'Perserver'
+        LicenseMaxConnections = 5
+        NamingScheme          = 'Fixed'
+        NamingPrefix          = $vmname
+    }
 }
-else {
 
+$vcenter_pass_sec = ConvertTo-SecureString $vcenter_pass -AsPlainText -Force
+$vcenter_cred = New-Object System.Management.Automation.PSCredential ($vcenter_user, $vcenter_pass_sec)
+Get-Module -ListAvailable VMware* | Import-Module -ErrorAction SilentlyContinue | Out-Null
+$connection = Connect-VIServer -Server $vcenter -Credential $vcenter_cred | Out-Null
+New-OSCustomizationSpec @splat | Out-Null
+sleep -Seconds 5
+try {
+    $OSCustomizationSpec = Get-OSCustomizationSpec $cspec_name -ErrorAction Stop
+    Write-Output $OSCustomizationSpec
+    Disconnect-VIServer -Force -Confirm:$false | Out-Null
 }
-Write-Output '---------'
-Write-Output $disk_cfg_file_contents
-Write-Output '---------'
-Write-Output $networking_cfg_file_contents
-Write-Output '---------'
-foreach ($disk in $disk_cfg.disks) {
-    Write-Output "The label is $($disk.label)"
+catch {
+    Disconnect-VIServer -Force -Confirm:$false | Out-Null
+    exit 1
 }
-Write-Output '---------'
-Write-Output $disk_cfg.disks | fl *
-Write-Output '---------'
-Write-Output $disk_cfg.disks
