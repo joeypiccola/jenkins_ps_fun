0374641e45685362b856b47d5b9e56a2e598e3dd
joeypiccola <joey.piccola@gmail.com>
2018-03-06 11:15:04 -0700

    added new-vm

---
+++ New-VM.ps1
@@ -0,0 +1,60 @@
+﻿[CmdletBinding()]
+Param (
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vcenter_user
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vcenter_pass
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vcenter
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vmname
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$datacenter
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$cluster
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$template_name
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$cspec_name
+    ,
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [pscustomobject]$disk_cfg
+)
+
+$ErrorActionPreference = 'Stop'
+$InformationPreference = 'Continue'
+$WarningPreference = 'Continue'
+
+# load helper functions
+. .\helperFunctions.ps1
+
+# connect to vmware
+$vcenter_pass_sec = ConvertTo-SecureString $vcenter_pass -AsPlainText -Force
+$vcenter_cred = New-Object System.Management.Automation.PSCredential ($vcenter_user, $vcenter_pass_sec)
+Get-Module -ListAvailable VMware* | Import-Module
+Connect-VIServer -Server $vcenter -Credential $vcenter_cred
+
+try {
+    $dsObject = Get-DatastoreClusters -Cluster $cluster | ?{$_.name -eq $disk_cfg.datastore}
+    $location = Get-Folder -Name '~NEW DEPLOYMENTS' -Location $datacenter
+    New-VM `
+        -Name $vmname `
+        -Template $template_name `
+        -ResourcePool $cluster `
+        -OSCustomizationSpec $cspec_name `
+        -Location $location `
+        -Datastore $dsObject
+    sleep -Seconds 5
+    Get-VM -Name $vmname
+} catch {
+    Write-Error $_.Exception.Message
+} finally {
+    Disconnect-VIServer -Force -Confirm:$false
+}
