b13d4bf5441d5b444fcd4f45d90d4857cbb68348
joeypiccola <joey.piccola@gmail.com>
2018-03-10 21:43:09 -0700

    added invoke vmscript

---
+++ Invoke-VMScript.ps1
@@ -0,0 +1,81 @@
+[CmdletBinding()]
+Param (
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vcenter_user
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vcenter_pass
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$ad_user	
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$ad_pass
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$localadmin_user	
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$localadmin_pass
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vcenter
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$vmname
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$role
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$win_domain
+    ,
+    [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+    [string]$cspec_name
+    ,
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [pscustomobject]$disk_cfg
+    ,
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [pscustomobject]$networking_cfg
+)
+
+$ErrorActionPreference = 'Stop'
+$InformationPreference = 'Continue'
+$WarningPreference = 'Continue'
+
+# connect to vmware
+$vcenter_pass_sec = ConvertTo-SecureString $vcenter_pass -AsPlainText -Force
+$vcenter_cred = New-Object System.Management.Automation.PSCredential ($vcenter_user, $vcenter_pass_sec)
+Get-Module -ListAvailable VMware* | Import-Module
+Connect-VIServer -Server $vcenter -Credential $vcenter_cred
+
+ # build guest credential
+$localadmin_pass_sec = ConvertTo-SecureString $localadmin_pass -AsPlainText -Force
+$guest_cred = New-Object System.Management.Automation.PSCredential ($localadmin_user, $localadmin_pass_sec)
+
+# take the provided disk config and convert it to a flat json string
+$disk_cfg_file_contents = $disk_cfg | ConvertTo-Json -Compress
+
+# take the provided networking config and convert it to a flat json string
+$networking_cfg_file_contents = $networking_cfg | ConvertTo-Json -Compress
+
+# take the provided role and replace the spaces with an underscore
+$role_script = $role.Replace(' ','_').ToLower()
+
+# build invoke script
+$script = @"
+Add-Content -Path C:\deploy\diskcfg.json -Value `'$disk_cfg_file_contents`'
+Add-Content -Path C:\deploy\netcfg.json -Value `'$networking_cfg_file_contents`'
+iwr http://nuget.ad.piccola.us:8081/chocogit.ps1 -UseBasicParsing | iex
+& 'c:\Program Files\Git\cmd\git.exe' clone -b master https://github.com/joeypiccola/vmware-runonce.git c:\deploy\config
+. c:\deploy\config\$win_domain\$role_script.ps1
+"@
+
+try {
+    Invoke-VMScript -VM $vmname -GuestCredential $guest_cred -ScriptType Powershell -ScriptText $script -RunAsync
+} catch {
+    Write-Error $_.Exception.Message
+} finally {
+    Disconnect-VIServer -Force -Confirm:$false
+}
